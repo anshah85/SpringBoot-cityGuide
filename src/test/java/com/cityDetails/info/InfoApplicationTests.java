efc462f439a7b87d1658ea4310febef3a1949bd0
Akshat Shah <ashah85@binghamton.edu>
2024-05-04 02:03:53 -0400

    First commit

---
+++ src/test/java/com/cityDetails/info/InfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityDetails.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
